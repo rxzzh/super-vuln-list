0cc8c4e9a366c117a650126262f2e48b0e5e21da
KuboRenka <ZhihangZhang1997@Gmail.com>
2021-07-08 06:41:31 +0800

    db progressing

--- utils.py
+++ utils.py
@@ -39,3 +39,11 @@ def gadget_set_row_height(rows):
     for row in rows:
         row.height = Cm(0.8)
         row.height_rule = WD_ROW_HEIGHT_RULE.AT_LEAST
+
+def singleton(class_):
+    instances = {}
+    def getinstance(*args, **kwargs):
+        if class_ not in instances:
+            instances[class_] = class_(*args, **kwargs)
+        return instances[class_]
+    return getinstance
